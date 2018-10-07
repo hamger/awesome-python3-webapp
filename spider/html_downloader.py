49977005915d75720f9d7d92434285b77963736e
hamger <2313303800@qq.com>
2018-10-07 15:58:08 +0800

    simple spider

---
+++ spider/html_downloader.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.7
+# -*- coding: utf-8 -*-
+import string
+from urllib import request as req
+from urllib.parse import quote
+
+class HtmlDownloader(object):
+  def download(self, url):
+    if url is None:
+      return None
+    url_ = quote(url, safe=string.printable)
+    response = req.urlopen(url_)
+    # response = req.urlopen(url)
+    if response.getcode() != 200:
+      return None
+    
+    return response.read()
