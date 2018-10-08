b4809b691776267ebb1d189e11cbbfe48a331c92
hamger <2313303800@qq.com>
2018-10-08 16:08:06 +0800

    add ipcheck

--- douban/douban/items.py
+++ douban/douban/items.py
@@ -7,11 +7,12 @@
 
 import scrapy
 
+
 class DoubanItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     serial_number = scrapy.Field()
-    movie_name =  scrapy.Field()
+    movie_name = scrapy.Field()
     introduce = scrapy.Field()
     star = scrapy.Field()
     descripe = scrapy.Field()
--- other/guifirst.py
+++ other/guifirst.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from tkinter import * 
+from tkinter import *
 import tkinter.messagebox as messagebox 
 
 class Application(Frame):
